883849d358fb833a55cb9a282738fd833340b69f
Terencio Martins <terencio.martins@3cplusnow.com>
2024-07-03 11:52:23 -0300

    build: change dockerfile and readme

--- README.md
+++ README.md
@@ -19,47 +19,11 @@ Siga estas etapas para configurar o projeto em sua máquina local.
    git clone https://github.com/TerencioMartins/icasei-test.git
    cd icasei-test
    ```
-2. Instale as dependências do projeto.
-   Execute os seguintes comandos na raiz do projeto para instalar as dependências necessárias para o backend e o frontend.
-   Para o backend (BFF):
-
-```
-cd bff
-npm install
-cd ..
-```
-
-Para o frontend (container):
-
-```
-cd container
-npm install
-cd ..
-```
-
-# Compilando o Projeto
-
-Para compilar o projeto, você precisa compilar tanto o backend quanto o frontend. Siga estas etapas:
-
-1. Compilar o backend:
-
-```
-cd bff
-npm run build
-cd ..
-```
-
-2. Compilar o frontend:
-
-```
-cd container
-npm run build
-cd ..
-```
 
 # Executando o Projeto
 
-Após compilar o projeto, você pode executá-lo usando Docker Compose.
+Após clonar o projeto, você pode executá-lo usando Docker Compose.
+Ps. Lembre-se de estar com o docker aberto na sua máquina.
 
 1. Na raiz do projeto, execute o seguinte comando para iniciar todos os serviços definidos no docker-compose.yml:
 
@@ -77,14 +41,18 @@ Para executar os testes, você precisa executar os testes tanto para o backend q
 
 ```
 cd bff
+npm install
 npm test
+cd ..
 ```
 
 2. Testes do frontend:
 
 ```
 cd container
+npm install
 npm test
+cd ..
 ```
 
 Siga estas instruções para compilar, testar e executar o projeto. Se você encontrar algum problema, verifique se todos os requisitos estão instalados corretamente e se os comandos foram executados na raiz correta do projeto.
--- container/src/mf_videos/mf_videos.ts
+++ container/src/mf_videos/mf_videos.ts
@@ -21,24 +21,19 @@ class MFVideos extends HTMLElement {
   private addEventListeners(): void {
     this.videoSearchInput.addEventListener("input", (e: Event) => {
       const target = e.target as HTMLInputElement;
-      const isEmpty = target.value.length === 0;
-      this.toggleSearchIconState(isEmpty);
     });
     this.searchIcon.addEventListener("click", () => this.onVideoSearchClick());
+    this.videoSearchInput.addEventListener("keydown", (e: KeyboardEvent) => {
+      if (e.key === "Enter") {
+        this.onVideoSearchClick();
+      }
+    });
   }
 
   private onVideoSearchClick(): void {
     const query = this.videoSearchInput.value;
     if (query) {
       this.searchVideos(query);
-    }
-  }
-
-  private toggleSearchIconState(disable: boolean): void {
-    if (disable) {
-      this.searchIcon.classList.add("disabled");
-    } else {
-      this.searchIcon.classList.remove("disabled");
     }
   }
 
@@ -118,9 +113,9 @@ class MFVideos extends HTMLElement {
     this.root.innerHTML = `
       <style>@import url('/mf_videos/mf_videos.css');</style>
       <div class="mf_videos_container">
-        <div id="mf_videos_search_input" class="input-container disabled">
+        <div id="mf_videos_search_input" class="input-container">
             <input type="text" id="video-search" class="input-box" placeholder="Pesquisar">
-            <svg id="mf_videos_search_input-icon" class="disabled" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24">
+            <svg id="mf_videos_search_input-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewbox="0 0 24 24">
                 <path d="M14.718,18.155l5.11,5.127c0.954,0.957 2.503,0.957 3.457,0c0.953,-0.957 0.953,-2.511 -0,-3.468l-5.131,-5.147c0.836,-1.441 1.315,-3.115 1.315,-4.901c-0,-5.39 -4.362,-9.766 -9.735,-9.766c-5.372,0 -9.734,4.376 -9.734,9.766c-0,5.39 4.362,9.766 9.734,9.766c1.821,-0 3.526,-0.503 4.984,-1.377Zm-4.984,-15.723c4.035,0 7.31,3.286 7.31,7.334c0,4.048 -3.275,7.334 -7.31,7.334c-4.034,-0 -7.31,-3.286 -7.31,-7.334c0,-4.048 3.276,-7.334 7.31,-7.334Z"/>
             </svg>
         </div>
